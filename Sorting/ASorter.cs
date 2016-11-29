469d3ed9027ab3694387d86cdaf118577730dca9
Brett LaRose <brett_larose@live.ca>
2016-11-29 13:06:54 -0600

    First commit and push

---
+++ Sorting/ASorter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public abstract class ASorter<T> where T: IComparable<T>
+    {
+        //Attributes
+        protected T[] array;
+
+        //Constructor
+        public ASorter(T[] array)
+        {
+            this.array = array;
+        }
+
+        //Abstract methods
+        public abstract void Sort();
+
+        //Helper methods
+
+        /// <summary>
+        /// Swap two values in the array
+        /// </summary>
+        /// <param name="first">Location of first item to swap</param>
+        /// <param name="second">Location/index of the second item to swap</param>
+        protected void Swap(int first, int second)
+        {
+
+        }
+    }
+}
